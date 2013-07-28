aa32e16f1f858a3cca67f65283c3c6b03bdf54fb
jiung kang <trkang79@gmail.com>
2013-07-29 00:15:27 +0900

    mongodb server를 mongohq.com으로 변경했습니다.

--- routes/questionModel.js
+++ routes/questionModel.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var mySchema = Schema({ title: String, content: String });
 
-var uri = 'mongodb://localhost/CodingInterview';
+var uri = 'mongodb://codinginterview:123456@dharma.mongohq.com:10005/CodingInterview';
 var db = mongoose.createConnection(uri);
-// db is global
+
 module.exports = db.model('question', mySchema);
